35204f52e81aa5b8f99c863d35456ca18277433e
Shubhayu Shrestha <shresthashubhayu@gmail.com>
2024-03-20 20:28:49 -0500

    adding starter files

---
+++ src/main/java/com/example/datadistributor/DataDistributorApplication.java
@@ -0,0 +1,13 @@
+package com.example.datadistributor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataDistributorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataDistributorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/datadistributor/DataDistributorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.datadistributor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataDistributorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
