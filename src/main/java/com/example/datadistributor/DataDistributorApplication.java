7c1eca79f6b662d628dc277c14d6b38737c87bbd
Shubhayu Shrestha <shresthashubhayu@gmail.com>
2024-03-20 23:11:57 -0500

    adding student class

--- src/main/java/com/example/datadistributor/DataDistributorApplication.java
+++ src/main/java/com/example/datadistributor/DataDistributorApplication.java
@@ -1,13 +1,35 @@
 package com.example.datadistributor;
 
+import com.example.datadistributor.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class DataDistributorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DataDistributorApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DataDistributorApplication.class, args);
+    }
+
+    @GetMapping
+    public List<Student> hello() {
+        return List.of(
+                new Student(
+                        1L,
+                        "Shubhayu Shrestha",
+                        "shubshresdev@gmail.com",
+                        23,
+                        LocalDate.of(2000, Month.DECEMBER, 29)
+
+                )
+        );
+    }
 
 }
