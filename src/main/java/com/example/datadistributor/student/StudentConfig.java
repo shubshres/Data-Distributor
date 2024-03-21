0bd7699bb4f7b97aaa1523966d32d8a39b1ee8ba
Shubhayu Shrestha <shresthashubhayu@gmail.com>
2024-03-21 11:49:45 -0500

    saving students to db

---
+++ src/main/java/com/example/datadistributor/student/StudentConfig.java
@@ -0,0 +1,37 @@
+package com.example.datadistributor.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean // object that is instantiated, assembled, and managed by Spring Container
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student shubhayu = new Student(
+                    1L,
+                    "Shubhayu Shrestha",
+                    "shubshresdev@gmail.com",
+                    23,
+                    LocalDate.of(2000, Month.DECEMBER, 29)
+
+            );
+
+            Student rohan = new Student(
+                    "Rohan Shrestha",
+                    "rohanshrestha@gmail.com",
+                    23,
+                    LocalDate.of(2005, Month.JANUARY, 21)
+
+            );
+
+            repository.saveAll(List.of(shubhayu, rohan));
+        };
+    }
+}
